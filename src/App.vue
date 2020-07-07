67ea98378f7c2e8506d60de1ba9073501ce74997
chenchen <729041629@qq.com>
2020-07-07 23:51:34 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <div>测试docker</div>
+    <div>测试dock11er</div>
   </div>
 </template>
 
