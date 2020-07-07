8c4f5640fc7630ead85e1a96caf2883980914d82
chenchen <729041629@qq.com>
2020-07-07 23:44:07 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <div>ssccdds</div>
+    <div>测试docker</div>
   </div>
 </template>
 
