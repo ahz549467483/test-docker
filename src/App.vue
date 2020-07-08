6d6041f390f6a4e65511d926693a7e0a4631491a
chenchen <729041629@qq.com>
2020-07-08 08:00:51 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <div>测试docr</div>
+    <div>测试docrk</div>
   </div>
 </template>
 
