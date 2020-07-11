b0fc6d8cbd8d98c0dfe70843efe2c33e79f656a3
chenchen <729041629@qq.com>
2020-07-11 22:53:30 +0800

    sasd

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <div>xxsssssx</div>
+    <div>xxsssssssx</div>
   </div>
 </template>
 
